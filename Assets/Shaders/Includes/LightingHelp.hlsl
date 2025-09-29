041a8515a029b96e0d08da5ebc181e27ae8626c0
grievans <grievans@users.noreply.github.com>
2025-09-29 19:30:54 -0400

    Files from Planning; files from lab; multiple light support; rim highlight shader concept

--- Assets/Shaders/Includes/LightingHelp.hlsl
+++ Assets/Shaders/Includes/LightingHelp.hlsl
@@ -21,6 +21,65 @@ void GetMainLight_float(float3 WorldPos, out float3 Color, out float3 Direction,
 #endif
 }
 
+
+#if 1
+void ComputeAdditionalLighting_float(float3 WorldPosition, float3 WorldNormal,
+    float2 Thresholds, float3 RampedDiffuseValues,
+    out float3 Color, out float Diffuse)
+{
+    Color = float3(0, 0, 0);
+    Diffuse = 0;
+
+#ifndef SHADERGRAPH_PREVIEW
+
+    int pixelLightCount = GetAdditionalLightsCount();
+    
+    for (int i = 0; i < pixelLightCount; ++i)
+    {
+        Light light = GetAdditionalLight(i, WorldPosition);
+        float4 tmp = unity_LightIndices[i / 4];
+        uint light_i = tmp[i % 4];
+
+        half shadowAtten = light.shadowAttenuation * AdditionalLightRealtimeShadow(light_i, WorldPosition, light.direction);
+        
+        half NdotL = saturate(dot(WorldNormal, light.direction));
+        half distanceAtten = light.distanceAttenuation;
+
+        half thisDiffuse = distanceAtten * shadowAtten * NdotL;
+        
+        half rampedDiffuse = 0;
+        
+        if (thisDiffuse < Thresholds.x)
+        {
+            rampedDiffuse = RampedDiffuseValues.x;
+        }
+        else if (thisDiffuse < Thresholds.y)
+        {
+            rampedDiffuse = RampedDiffuseValues.y;
+        }
+        else
+        {
+            rampedDiffuse = RampedDiffuseValues.z;
+        }
+        
+        
+        if (shadowAtten * NdotL == 0)
+        {
+            rampedDiffuse = 0;
+
+        }
+        
+        if (light.distanceAttenuation <= 0)
+        {
+            rampedDiffuse = 0.0;
+        }
+
+        Color += max(rampedDiffuse, 0) * light.color.rgb;
+        Diffuse += rampedDiffuse;
+    }
+#endif
+}
+#else
 void ComputeAdditionalLighting_float(float3 WorldPosition, float3 WorldNormal,
     float2 Thresholds, float3 RampedDiffuseValues,
     out float3 Color, out float Diffuse)
@@ -78,6 +137,7 @@ void ComputeAdditionalLighting_float(float3 WorldPosition, float3 WorldNormal,
     
 #endif
 }
+#endif
 
 void ChooseColor_float(float3 Highlight, float3 Midtone, float3 Shadow, float Diffuse, float2 Thresholds, out float3 OUT)
 {
@@ -93,4 +153,124 @@ void ChooseColor_float(float3 Highlight, float3 Midtone, float3 Shadow, float Di
     {
         OUT = Highlight;
     }
+}
+
+
+
+
+
+// TODO kinda getting what I want but not 100% satisfied. unsure if I wanna include distance
+void ComputeRimLighting_float(float3 WorldPosition, float3 WorldNormal,
+    float2 Thresholds, float3 RampedDiffuseValues, bool useWorldLight, bool useDistanceAttenuation, bool useRamping,
+    out float3 Color, out float Diffuse)
+{
+    Color = float3(0, 0, 0);
+    Diffuse = 0;
+
+#ifndef SHADERGRAPH_PREVIEW
+
+    int pixelLightCount = GetAdditionalLightsCount();
+    
+    #if SHADOWS_SCREEN
+        float4 clipPos = TransformWorldToClip(WorldPosition);
+        float4 shadowCoord = ComputeScreenPos(clipPos);
+    #else
+        float4 shadowCoord = TransformWorldToShadowCoord(WorldPosition);
+    #endif
+
+    if (useWorldLight) {
+        Light light = GetMainLight(shadowCoord);
+        half shadowAtten = light.shadowAttenuation;
+        
+        half NdotL = saturate(1.f - abs(dot(WorldNormal, light.direction)));
+        half distanceAtten = light.distanceAttenuation;
+
+        // half thisDiffuse = NdotL * distanceAtten;
+        half thisDiffuse = (useDistanceAttenuation ? distanceAtten : 1.f) * shadowAtten * NdotL;
+        
+        half rampedDiffuse = 0;
+        if (useRamping) {
+            
+            if (thisDiffuse < Thresholds.x)
+            {
+                rampedDiffuse = RampedDiffuseValues.x;
+            }
+            else if (thisDiffuse < Thresholds.y)
+            {
+                rampedDiffuse = RampedDiffuseValues.y;
+            }
+            else
+            {
+                rampedDiffuse = RampedDiffuseValues.z;
+            }
+            
+        } else {
+            rampedDiffuse = thisDiffuse;
+        }
+        // rampedDiffuse = thisDiffuse;
+        
+        // if (shadowAtten * NdotL == 0)
+        // {
+        //     rampedDiffuse = 0;
+
+        // }
+        
+        // if (light.distanceAttenuation <= 0)
+        // {
+        //     rampedDiffuse = 0.0;
+        // }
+
+        Color += max(rampedDiffuse, 0) * light.color.rgb;
+        Diffuse += rampedDiffuse;
+    }
+
+    for (int i = 0; i < pixelLightCount; ++i)
+    {
+        Light light = GetAdditionalLight(i, WorldPosition);
+        float4 tmp = unity_LightIndices[i / 4];
+        uint light_i = tmp[i % 4];
+
+        half shadowAtten = light.shadowAttenuation * AdditionalLightRealtimeShadow(light_i, WorldPosition, light.direction);
+        
+        half NdotL = saturate(1.f - abs(dot(WorldNormal, light.direction)));
+        half distanceAtten = light.distanceAttenuation;
+
+        half thisDiffuse = NdotL * shadowAtten * (useDistanceAttenuation ? distanceAtten : 1.f);
+        // half thisDiffuse = distanceAtten * shadowAtten * NdotL;
+        
+        half rampedDiffuse = 0;
+        
+        if (useRamping) {
+            
+            if (thisDiffuse < Thresholds.x)
+            {
+                rampedDiffuse = RampedDiffuseValues.x;
+            }
+            else if (thisDiffuse < Thresholds.y)
+            {
+                rampedDiffuse = RampedDiffuseValues.y;
+            }
+            else
+            {
+                rampedDiffuse = RampedDiffuseValues.z;
+            }
+            
+        } else {
+            rampedDiffuse = thisDiffuse;
+        }
+        // if (shadowAtten * NdotL == 0)
+        // {
+        //     rampedDiffuse = 0;
+
+        // }
+        
+        // if (light.distanceAttenuation <= 0)
+        // {
+        //     rampedDiffuse = 0.0;
+        // }
+
+        Color += max(rampedDiffuse, 0) * light.color.rgb;
+        Diffuse += rampedDiffuse;
+    }
+#endif
 }
