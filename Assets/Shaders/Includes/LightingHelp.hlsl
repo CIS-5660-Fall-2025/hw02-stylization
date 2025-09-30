071df807ac9ce7f7a6cf88d910849e990d7aa7df
RFK <kakigori11235@gmail.com>
2025-09-29 23:47:36 -0400

    kind of painterly

--- Assets/Shaders/Includes/LightingHelp.hlsl
+++ Assets/Shaders/Includes/LightingHelp.hlsl
@@ -94,3 +94,44 @@ void ChooseColor_float(float3 Highlight, float3 Midtone, float3 Shadow, float Di
         OUT = Highlight;
     }
 }
+
+void ChooseTriColor_float(float3 Highlight, float3 Shadow, float Diffuse, float3 Midtone, float Threshold, float Threshold2, out float3 OUT)
+{
+    if (Diffuse < Threshold)
+    {
+        OUT = Shadow;
+    }
+    else if (Diffuse < Threshold2)
+    {
+        OUT = Midtone;
+    }
+    else
+    {
+        OUT = Highlight;
+    }
+}
+
+void ChooseTriColorSmooth_float(float3 Highlight, float3 Shadow, float Diffuse, float3 Midtone, float Threshold, float Threshold2, float Smoothness, out float3 OUT)
+{
+    float smoothness = 0.1;
+
+    float t0 = smoothstep(Threshold - smoothness, Threshold + smoothness, Diffuse);
+    float3 shadowToMidtone = lerp(Shadow, Midtone, t0);
+
+    float t1 = smoothstep(Threshold2 - smoothness, Threshold2 + smoothness, Diffuse);
+
+    OUT = lerp(shadowToMidtone, Highlight, t1);
+}
+
+void addStripes_float(float shadowAtten, float3 Shadow, float3 Currtone, float stripes, out float3 OUT)
+{
+    if (shadowAtten < 0.3)
+    {
+        OUT = Shadow * (1 - stripes) + Currtone * stripes;
+    }
+    else
+    {
+        OUT = Currtone;
+    }
+
+}
