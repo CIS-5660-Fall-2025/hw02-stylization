5dd67bdcef3fbd499b5c1b56872d68cd7aae4aae
Aidan <aidannn@seas.upenn.edu>
2025-09-30 23:57:48 -0400

    TV Glitch

--- Assets/Shaders/Includes/TVHelp.hlsl
+++ Assets/Shaders/Includes/TVHelp.hlsl
@@ -1,6 +1,44 @@
+float cubicPulse(float c, float w, float x) { // From toolbox slides
+    x = abs(x);
+    if(x >w) return 0.;
+    x /= w;
+    return 1.-x*x*(3.-2.*x);
+}
+
 float stripe(float x, float rep, float stripeFac) {
     return step(abs(fmod(x, rep) - rep*.5), rep*.5*stripeFac);
 }
+
+float hash11(float p) // From https://www.shadertoy.com/view/4djSRW
+{
+    p = frac(p * .1031);
+    p *= p + 33.33;
+    p *= p + p;
+    return frac(p);
+}
+
+void SampleTVBrightness_float(float Time, out float BrightnessFac) {
+    const float repTime = 1.;
+    const float pulseFac = 0.78;
+
+    float lt = fmod(Time, repTime) - repTime*.5;
+    lt /= repTime*.5;
+    float pulse = cubicPulse(0., pulseFac, lt);
+
+    BrightnessFac = pulse;//sin(Time*10.)*.5+.5;//pulse;
+}
+
+void DistortTVUV_float(float2 UV, float StripeRepSize, float OffsetMultiplier, float HashOffset, out float2 UVOutput) {
+    float2 p = UV + float2(0.,1000.);
+
+    float yID = StripeRepSize*floor(p.y/StripeRepSize);
+    float off = hash11(yID+HashOffset)*2.-1.;
+    off *= OffsetMultiplier;
+
+    UVOutput = UV + float2(off, 0.);
+}
+
+
 
 void SampleTV_float(float2 UV, float TVScale, float Offset, out float Opacity) {
     float2 p = UV;
