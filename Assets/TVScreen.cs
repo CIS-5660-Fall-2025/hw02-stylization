fbe9cffcf47a960ff9d44a89c9ce9bb6f87a4afe
Aidan <aidannn@seas.upenn.edu>
2025-10-05 13:47:51 -0400

    Color Array

--- Assets/TVScreen.cs
+++ Assets/TVScreen.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class TVScreen : MonoBehaviour
 {
+    [SerializeField] Color[] screenColors;
+
     void Start()
     {
         var renderer = GetComponent<MeshRenderer>();
         var propBlock = new MaterialPropertyBlock();
         renderer.GetPropertyBlock(propBlock);
-        propBlock.SetColor("_InnerColor", Color.HSVToRGB(Random.Range(-0.2f, 0.2f) + 5f/6f, .68f, 1f));
+        propBlock.SetColor("_InnerColor", screenColors[Random.Range(0, screenColors.Length)]);
+        //propBlock.SetColor("_InnerColor", Color.HSVToRGB(Random.Range(-0.2f, 0.2f) + 5f/6f, .68f, 1f));
         renderer.SetPropertyBlock(propBlock);
     }
 }
