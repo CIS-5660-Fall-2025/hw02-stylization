c4eccb3bda5255b34133d8b3418c97cf6b3ff2fa
Eli Asimow <57159792+eliasimow@users.noreply.github.com>
2025-10-06 14:10:26 -0400

    wtf

--- Assets/Render Settings/Render Features/Full Screen Feature.cs
+++ Assets/Render Settings/Render Features/Full Screen Feature.cs
@@ -3,25 +3,21 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
-public class FullScreenFeature : ScriptableRendererFeature
-{
+public class FullScreenFeature : ScriptableRendererFeature {
     [System.Serializable]
-    public class FullScreenPassSettings
-    {
+    public class FullScreenPassSettings {
         public RenderPassEvent renderPassEvent = RenderPassEvent.AfterRenderingTransparents;
         public Material material;
     }
 
     [SerializeField] private FullScreenPassSettings settings;
-    class FullScreenPass : ScriptableRenderPass
-    {
+    class FullScreenPass : ScriptableRenderPass {
         const string ProfilerTag = "Full Screen Pass";
         public FullScreenFeature.FullScreenPassSettings settings;
         RenderTargetIdentifier colorBuffer, temporaryBuffer;
         private int temporaryBufferID = Shader.PropertyToID("_TemporaryBuffer");
 
-        public FullScreenPass(FullScreenFeature.FullScreenPassSettings passSettings)
-        {
+        public FullScreenPass(FullScreenFeature.FullScreenPassSettings passSettings) {
             this.settings = passSettings;
             this.renderPassEvent = settings.renderPassEvent;
             if (settings.material == null) settings.material = CoreUtils.CreateEngineMaterial("Shader Graphs/Invert");
@@ -32,8 +28,7 @@ public class FullScreenFeature : ScriptableRendererFeature
         // When empty this render pass will render to the active camera render target.
         // You should never call CommandBuffer.SetRenderTarget. Instead call <c>ConfigureTarget</c> and <c>ConfigureClear</c>.
         // The render pipeline will ensure target setup and clearing happens in a performant manner.
-        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
-        {
+        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData) {
             RenderTextureDescriptor descriptor = renderingData.cameraData.cameraTargetDescriptor;
             colorBuffer = renderingData.cameraData.renderer.cameraColorTarget;
 
@@ -45,23 +40,25 @@ public class FullScreenFeature : ScriptableRendererFeature
         // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
         // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
         // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
-        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
-        {
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData) {
             CommandBuffer cmd = CommandBufferPool.Get();
-            using (new ProfilingScope(cmd, new ProfilingSampler(ProfilerTag)))
-            {
+            using (new ProfilingScope(cmd, new ProfilingSampler(ProfilerTag))) {
                 // HW 4 Hint: Blit from the color buffer to a temporary buffer and *back*.
                 Blit(cmd, colorBuffer, temporaryBuffer, settings.material);
+
+                Blit(cmd, temporaryBuffer, colorBuffer);
+
             }
 
             // Execute the command buffer and release it.
             context.ExecuteCommandBuffer(cmd);
+
+
             CommandBufferPool.Release(cmd);
         }
 
         // Cleanup any allocated resources that were created during the execution of this render pass.
-        public override void OnCameraCleanup(CommandBuffer cmd)
-        {
+        public override void OnCameraCleanup(CommandBuffer cmd) {
             if (cmd == null) throw new ArgumentNullException("cmd");
             cmd.ReleaseTemporaryRT(temporaryBufferID);
         }
@@ -70,15 +67,13 @@ public class FullScreenFeature : ScriptableRendererFeature
     FullScreenPass m_FullScreenPass;
 
     /// <inheritdoc/>
-    public override void Create()
-    {
+    public override void Create() {
         m_FullScreenPass = new FullScreenPass(settings);
     }
 
     // Here you can inject one or multiple render passes in the renderer.
     // This method is called when setting up the renderer once per-camera.
-    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
-    {
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData) {
         if (renderingData.cameraData.cameraType != CameraType.Game)
             return;
         renderer.EnqueuePass(m_FullScreenPass);
